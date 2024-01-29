ab6e91b31b390a0bce8bfb61ceabc519d290871c
Farhan <kuuhaku.1332@gmail.com>
2024-01-29 15:19:12 +0700

    add: latihan audio player

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,106 @@
+import 'package:audioplayer_project/provider/audio_notifier.dart';
+import 'package:audioplayer_project/utils/utils.dart';
+import 'package:audioplayer_project/widget/audio_controller_widget.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widget/buffer_slider_controller_widget.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final AudioPlayer audioPlayer;
+  late final Source audioSource;
+
+  @override
+  void initState() {
+    final provider = context.read<AudioNotifier>();
+
+    audioPlayer = AudioPlayer();
+    audioSource = AssetSource('cricket.wav');
+    audioPlayer.setSource(audioSource);
+
+    audioPlayer.onPlayerStateChanged.listen((state) {
+      provider.isPlay = state == PlayerState.playing;
+    });
+
+    audioPlayer.onDurationChanged.listen((duration) {
+      provider.duration = duration;
+    });
+
+    audioPlayer.onPositionChanged.listen((position) {
+      provider.position = position;
+    });
+
+    audioPlayer.onPlayerComplete.listen((_) {
+      provider.position = Duration.zero;
+      provider.isPlay = false;
+    });
+
+    audioPlayer.onPlayerStateChanged.listen((state) {
+      provider.isPlay = state == PlayerState.playing;
+      if (state == PlayerState.stopped) {
+        provider.position = Duration.zero;
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Audio Player Project"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Consumer<AudioNotifier>(
+            builder: (context, provider, child) {
+              final duration = provider.duration;
+              final position = provider.position;
+
+              return BufferSliderControllerWidget(
+                maxValue: duration.inSeconds.toDouble(),
+                currentValue: position.inSeconds.toDouble(),
+                minText: durationToTimeString(position),
+                maxText: durationToTimeString(duration),
+                onChanged: (value) async {
+                  final newPosition = Duration(seconds: value.toInt());
+                  await audioPlayer.seek(newPosition);
+
+                  await audioPlayer.resume();
+                },
+              );
+            },
+          ),
+          Consumer<AudioNotifier>(
+            builder: (context, provider, child) {
+              final bool isPlay = provider.isPlay;
+              return AudioControllerWidget(
+                onPlayTapped: () => audioPlayer.play(audioSource),
+                onPauseTapped: () => audioPlayer.pause(),
+                onStopTapped: () => audioPlayer.stop(),
+                isPlay: isPlay,
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
